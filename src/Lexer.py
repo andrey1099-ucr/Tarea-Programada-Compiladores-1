1326bc9fdf1277c1411b80c66d0e6635df056e02
FelipeMena11 <felipe.menarodriguez@ucr.ac.cr>
2025-09-05 21:57:53 -0600

    Added utils file with Error class and defined simple tokens
    Co-authored-by: andrey1099-ucr <andrey1099-ucr@users.noreply.github.com>

--- src/Lexer.py
+++ src/Lexer.py
@@ -1,7 +1,16 @@
 import ply.lex as lex
 
 
+from src.utils import Error
 class Lexer:
+    def __init__(self, errors: list[Error], debug=False):
+        self.lex = None
+        self.data = None
+        self.debug = debug
+        self.reserved_map = {}
+        self.errors = errors  # to use the same list of errors that the parser uses
+        for r in self.reserved:
+            self.reserved_map[r.lower()] = r
 
     # Python's reserved words
     reserved = (
@@ -26,7 +35,11 @@ class Lexer:
     # Token for variables,functions and class names
     identifier = "ID"
 
-    literals = ("INTEGER", "FLOAT", "STRING")
+    literals = (
+        "INTEGER",
+        "FLOAT",
+        "STRING"
+    )
 
     operators = (
         # Arithmetic Operators
@@ -71,3 +84,47 @@ class Lexer:
 
     # All tokens
     tokens = reserved + literals + operators + delimiters_and_symbols + indentation
+    
+    # Regular expression rules for simple tokens
+    
+    # Ignore spaces and tabs
+    t_ignore = "\t"
+
+    t_ADD = r"\+"
+    t_MINUS = r"-"
+    t_TIMES = r"\*"
+    t_DIVIDE = r"/"
+    t_FLOORDIV = r"//"
+    t_MODULE = r"%"
+    t_POWER = r"\*\*"
+
+    t_EQUAL_EQUAL = r"=="
+    t_NOT_EQUAL = r"!="
+    t_LESS = r"<"
+    t_GREATER = r">"
+    t_LESS_EQUAL = r"<="
+    t_GREATER_EQUAL = r'>='
+
+    t_EQUAL = r'='
+    t_PLUS_EQUAL = r'\+='
+    t_MINUS_EQUAL = r'-='
+    t_TIMES_EQUAL = r'\*='
+    t_DIVIDE_EQUAL = r'/='
+    t_MODULE_EQUAL = r'%='
+    t_FLOORDIV_EQUAL = r'//='
+    t_POWER_EQUAL = r'\*\*='
+
+    t_LPAREN = r'\('
+    t_RPAREN = r'\)'
+    t_LBRACKET = r'\['
+    t_RBRACKET = r'\]'
+    t_LBRACE = r'\{'
+    t_RBRACE = r'\}'
+    t_COLON = r':'
+    t_COMMA = r','
+    t_DOT = r'\.'
+
+    # Functions for tokens
+    def t_COMMENT(self, t):
+        r"\#[^\n]*"
+        pass
